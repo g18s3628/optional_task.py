6c59a8afc30eab39feead9bfb45224f277746e24
g18s3628 <36521304+g18s3628@users.noreply.github.com>
2023-07-21 11:23:26 +0200

    optional_task.py

---
+++ optional_task.py
@@ -0,0 +1,21 @@
+Hi = "Hello to the world of syntax errors"
+print "Hi" #Syntax error: Missing parantheses
+bad = "wrong name"
+print("There is something wrong in this sentence, " + badd) #Syntax error: Wrong spelling for variable
+
+run_t = "Hello to the world of runtime errors"
+print(run_t)
+a = 3
+b = 4
+c = 37
+den = 0
+average = (a + b + c) / den #Runtime error: Dividing by zero
+
+log_e = "Hello to the world of logical errors"
+number1 = 8
+number2 = 3
+number3 = 7 
+number4 = 27
+number5 = 93
+averageloc = (number1 + number2 + number3 + number4 + number5) / 3 #Logical error: Incorrect formula for the average therefore wrong output 
+print("The average of the 5 numbers is:", averageloc) 
